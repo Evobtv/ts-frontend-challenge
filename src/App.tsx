38d4651e11de404493f1e197bd906eae19b4b8f7
manoel <manoeldiasmf@gmail.com>
2023-11-13 18:59:30 -0300

    TEST: app init and test styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: darkblue;
+  }
+`;
+
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -18,9 +32,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
