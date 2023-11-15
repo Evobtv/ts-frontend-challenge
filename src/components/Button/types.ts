b389919b1dda9b3018bb91f031e859194d6a292b
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2023-11-15 11:33:54 -0500

    feat: button component

---
+++ src/components/Button/types.ts
@@ -0,0 +1,12 @@
+import { ButtonHTMLAttributes } from 'react';
+
+export type ButtonVariant = 'primary' | 'secondary';
+
+export interface ButtonProps {
+  $variant?: ButtonVariant;
+  $fullWidth?: boolean;
+}
+
+export interface ButtonComponentProps
+  extends ButtonProps,
+    ButtonHTMLAttributes<HTMLButtonElement> {}
