6fd6aa474c6e2f266f8c5425d944ed4be4a56405
Ezequiel Soares <mr.ezequiel.soares@gmail.com>
2023-11-18 03:08:34 -0500

    feat: change path routers validations

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import GlobalStyles from './styles/GlobalStyles';
 import router, { RouterPath } from './router';
 
 export default function App() {
-  const path = window.location.pathname.split('/')[1] as RouterPath;
+  const path = window.location.pathname as RouterPath;
 
   return (
     <ThemeProvider theme={theme}>
-      {router[path] ? router[path]() : router[404]()}
+      {router[path] ? router[path]() : router['/404']()}
       <GlobalStyles />
     </ThemeProvider>
   );
--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,8 @@ import NotFound from './pages/NotFound';
 import Login from './pages/login';
 
 const router = {
-  login: Login,
-  '404': NotFound
+  '/login': Login,
+  '/404': NotFound
 } as const;
 
 export type RouterPath = keyof typeof router;
