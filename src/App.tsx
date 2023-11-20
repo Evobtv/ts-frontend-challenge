35c2abfc5e292de81addfffd748a8e9b3eba7e40
João Gabriel Vieira <joao.vieira@portotech.org>
2023-11-20 11:36:12 -0300

    feat: Global styles and theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-function App() {
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import variables from './assets/styles/themes/variables';
+import GlobalStyle from './assets/styles/global';
 
-
+const App: React.FC = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <ThemeProvider theme={variables}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
---
+++ src/assets/styles/themes/variables.ts
@@ -0,0 +1,17 @@
+const theme = {
+    colors: {
+        gray: {
+            0: '#2D3748',
+            1: '#F7FAFC',
+            2: '#A0AEC0',
+            3: '#4A5568',
+        },
+        white: '#FFFFFF',
+        black: '#000000',
+        success: '#28a745',
+        error: '#dc3545',
+    },
+};
+
+export type Theme = typeof theme;
+export default theme;
