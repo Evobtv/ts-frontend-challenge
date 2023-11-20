035c390f88df7923a3508e04f65dddfbceb4f44a
João Gabriel Vieira <joao.vieira@portotech.org>
2023-11-20 18:25:52 -0300

    feat: Router added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import variables from './assets/styles/themes/variables';
 import GlobalStyle from './assets/styles/global';
+import Router from './Router';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={variables}>
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   );
